e71efe6288f9d7e557870f3ad3d22d46b7781a68
ScrDesh <scrdesh+github@gmail.com>
2025-06-15 22:01:37 +0100

    In the gitted lab, straight up "pushing" it, and by it, lets just say, heh, squeezy

---
+++ src/client/kotlin/straight/squeezin/it/SqueezedClient.kt
@@ -0,0 +1,9 @@
+package straight.squeezin.it
+
+import net.fabricmc.api.ClientModInitializer
+
+object SqueezedClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
