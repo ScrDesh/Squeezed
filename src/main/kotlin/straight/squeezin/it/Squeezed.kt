30919a7ba1b58f691e76098466d95247b4e8b0f4
ScrDesh <scrdesh+github@gmail.com>
2025-06-17 14:48:16 +0100

    added all squeezed out

--- src/main/kotlin/straight/squeezin/it/Squeezed.kt
+++ src/main/kotlin/straight/squeezin/it/Squeezed.kt
@@ -1,6 +1,7 @@
 package straight.squeezin.it
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.fabricmc.fabric.api.registry.*
 import org.slf4j.LoggerFactory
 
@@ -11,7 +12,12 @@ object Squeezed : ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+		logger.info("All Squeezed Out.")
+
+		ServerLifecycleEvents.SERVER_STOPPING.register { server ->
+			logger.info("You don't know what they're like, you fall for 'em, you really love 'em, you think this is gonna be the biggest thing since The Graf Zeppelin! Then one morning you wake up, the guy's gone, the saxophone's gone, all that's left behind is a pair of old socks and a tube of toothpaste,\n" +
+					"All Squeezed Out.")
+		}
 		ModBlocks.initialize()
 		ModItems.initialize()
 		StrippableBlockRegistry.register(ModBlocks.BLIGHTED_LOG,ModBlocks.STRIPPED_BLIGHTED_LOG)
